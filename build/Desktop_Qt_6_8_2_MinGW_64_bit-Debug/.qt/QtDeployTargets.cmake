b98b788e95c4f9b6b8adb739a4860db41ebca192
Anastasiya <muravyeva.ab@yandex.ru>
2025-02-21 17:41:38 +0300

    Delete build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_login_window_FILE C:/Users/Nastya/Desktop/login_window/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/login_window.exe)
-set(__QT_DEPLOY_TARGET_login_window_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_login_window_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/MyProject_autogen/EWIEGA46WW/moc_registration_window.cpp.d
+++
@@ -1,2 +0,0 @@
-C:/Users/Nastya/Desktop/login_window/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/MyProject_autogen/EWIEGA46WW/moc_registration_window.cpp: C:/Users/Nastya/Desktop/login_window/registration_window.h \
-  C:/Users/Nastya/Desktop/login_window/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/MyProject_autogen/moc_predefs.h
