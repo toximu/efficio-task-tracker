0f3bd310cee6fb692b55f9719c4b3bec964cdc9e
mirovoretts <panoffilya@gmail.com>
2025-03-16 16:52:41 +0300

    refactor: improve the style

--- ui/note-widget/tags_dialog.h
+++ ui/note-widget/tags_dialog.h
@@ -8,10 +8,13 @@
 #include <QPushButton>
 #include <memory>
 
-class TagsDialog : public QDialog {
+class TagsDialog final : public QDialog {
     Q_OBJECT
 
 public:
+    const int MAX_TAGS_COUNT = 5;
+    const std::pair<int, int> DIALOG_SIZE = std::make_pair(300, 250);
+
     struct Tag {
         bool is_checked;
         QString color;
@@ -23,16 +26,16 @@ public:
         QWidget *parent = nullptr
     );
 
-    QList<Tag> get_selected_tags() const;
+    [[nodiscard]] QList<Tag> get_selected_tags() const;
 
 private:
     void setup_ui();
 
-    std::unique_ptr<QCheckBox> check_boxes[5];
-    std::unique_ptr<QComboBox> color_combo_boxes[5];
-    std::unique_ptr<QLineEdit> name_line_edits[5];
-    std::unique_ptr<QPushButton> ok_button;
-    std::unique_ptr<QPushButton> cancel_button;
+    std::unique_ptr<QCheckBox> check_boxes_[5];
+    std::unique_ptr<QComboBox> color_combo_boxes_[5];
+    std::unique_ptr<QLineEdit> name_line_edits_[5];
+    std::unique_ptr<QPushButton> ok_button_;
+    std::unique_ptr<QPushButton> cancel_button_;
 };
 
 #endif  // TAGS_DIALOG_H
