050b1cfd1c6771868f7786c22bd550ba2cd50c2b
toximu <timofei625211@gmail.com>
2025-04-12 01:11:06 +0300

    copy database to client

---
+++ client/database/database_manager.hpp
@@ -0,0 +1,23 @@
+#ifndef DATABASE_MANAGER_HPP
+#define DATABASE_MANAGER_HPP
+
+#include <QSqlQuery>
+
+class DatabaseManager final {
+public:
+    static DatabaseManager &get_instance();
+    [[nodiscard]] bool check_connection() const;
+    bool execute_query(
+        QSqlQuery &query,
+        const QString &query_str,
+        const QVariantList &params = {}
+    );
+
+private:
+    explicit DatabaseManager();
+    ~DatabaseManager();
+
+    QSqlDatabase database_;
+};
+
+#endif  // DATABASE_MANAGER_HPP
---
+++ client/database/lr_dao.hpp
@@ -0,0 +1,21 @@
+#ifndef LRDAO_H
+#define LRDAO_H
+
+#include <QByteArray>
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QString>
+#include "database_manager.hpp"
+
+class LRDao {
+public:
+    LRDao() = default;
+    static bool register_user(const QString &login, const QString &password);
+    static bool validate_user(const QString &login, const QString &password);
+
+private:
+    static QString hash_password(const QString &password);
+};
+
+#endif  // LRDAO_H
