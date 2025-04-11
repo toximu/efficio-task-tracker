12ac15f1516244c25c57a012b16e68f74c8fde47
toximu <timofei625211@gmail.com>
2025-04-12 00:56:31 +0300

    change .gitignore

---
+++ client/main.cpp
@@ -0,0 +1,30 @@
+#include <QApplication>
+#include <QLocale>
+#include <QMainWindow>
+#include <QTranslator>
+#include "applicationwindow.h"
+#include "mainwindow.h"
+#include "note.hpp"
+#include "storage.hpp"
+#include "registration_window.h"
+#include "login_window.h"
+
+using namespace Ui;
+
+int main(int argc, char *argv[]) {
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "MainWindow_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+
+    Ui::LoginWindow login_window;
+    login_window.show();
+    return login_window.exec();
+}
