2d417c05c10b40dcb2d5950fabf694792ee7f6a3
toximu <timofei625211@gmail.com>
2025-04-12 22:11:52 +0300

    try make request

--- client/client/src/client_implementation.cpp
+++ client/client/src/client_implementation.cpp
@@ -14,5 +14,27 @@ using grpc::Status;
 ClientImplementation::ClientImplementation(std::shared_ptr<Channel> channel) :
     channel_(channel) { };
 
+void ClientImplementation::CompleteRpc() {
 
+    void* got_tag;
+    bool ok = false;
+
+
+    while (cq_.Next(&got_tag, &ok)) {
+
+        CommonClientCall* call = static_cast<CommonClientCall*>(got_tag);
+
+        // Verify that the request was completed successfully. Note that "ok"
+        // corresponds solely to the request for updates introduced by Finish().
+        assert(ok);
+
+        if (call->status.ok())
+            call->Proceed();
+        else
+            std::cout << "RPC failed" << std::endl;
+
+        // Once we're complete, deallocate the call object.
+        delete call;
+    }
+}
 
--- client/client/src/update_requests.cpp
+++ client/client/src/update_requests.cpp
@@ -16,3 +16,26 @@ using Efficio_proto::GetNoteRequest;
 using Efficio_proto::GetNoteResponse;
 using Efficio_proto::Update;
 
+class UpdateRequests::GetProjectClientCall : public CommonClientCall {
+    GetProjectResponse response;
+    std::unique_ptr<ClientAsyncResponseReader<GetProjectResponse>> response_reader;
+
+    void Proceed(bool ok) override {
+
+        if (ok && status.ok()) {
+            if (response.has_error_string()) {
+                std::cout << response.error_string() << std::endl;
+            } else if (response.has_project()) {
+                std::cout << response.project().title() << std::endl;
+            }
+        }
+    }
+public:
+    GetProjectClientCall(const GetProjectRequest& request,
+        CompletionQueue& cq_,
+        std::unique_ptr<Update::Stub>& stub_) : CommonClientCall()
+    {
+        response_reader = stub_->AsyncGetProject(&context, request, &cq_);
+        response_reader->Finish(&response, &status, (void*)this);
+    }
+};
