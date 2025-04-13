b9c007d3fd961e68c235474a2460965b8b110383
mirotvoretts <panoffilya@gmail.com>
2025-04-13 21:57:14 +0300

    feat: add async Note calls and update server startup logic

--- server/Service/src/server_implementation.cpp
+++ server/Service/src/server_implementation.cpp
@@ -1,1 +1,28 @@
+#include "server_implementation.h"
+#include "common_server_call.h"
 
+using Efficio_proto::GetNoteRequest;
+
+void ServerImplementation::Run(const uint16_t port) {
+    grpc::ServerBuilder builder;
+    UpdateService update_service;
+
+    builder.AddListeningPort(
+        "0.0.0.0:" + std::to_string(port), grpc::InsecureServerCredentials()
+    );
+    builder.RegisterService(&update_service.get_service());
+
+    cq_ = builder.AddCompletionQueue();
+    server_ = builder.BuildAndStart();
+    HandleRPCs(update_service);
+}
+
+void ServerImplementation::HandleRPCs(UpdateService& update_service) const {
+    new UpdateService::GetNoteServerCall(update_service, cq_.get());
+
+    void *tag;
+    bool ok;
+    while (cq_->Next(&tag, &ok)) {
+        static_cast<CommonServerCall*>(tag)->Proceed(ok);
+    }
+}
--- server/main.cpp
+++ server/main.cpp
@@ -1,5 +1,7 @@
-
+#include "server_implementation.h"
 
 int main() {
-
+    ServerImplementation server;
+    server.Run(50051);
+    return 0;
 }
