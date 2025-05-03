0e772b49c0818a0f882e937554c9403be292582a
toximu <timofei625211@gmail.com>
2025-05-03 14:41:54 +0300

    rewrite main-window to protobuf

--- client/ui/main-window/include/projectitem.h
+++ client/ui/main-window/include/projectitem.h
@@ -1,19 +1,21 @@
 #ifndef PROJECTITEM_H
 #define PROJECTITEM_H
 
+#include <model-proto/model.pb.h>
 #include <QListWidget>
 #include <QObject>
 #include "notelist.h"
-#include "project.hpp"
+
+using namespace Efficio_proto;
 
 namespace Ui {
 class ProjectItem : public QListWidgetItem {
-    project_storage_model::Project *project_;
+    Project *project_;
     friend NoteList;
     friend class MainWindow;
 
 public:
-    ProjectItem(QListWidget *listview, project_storage_model::Project *project);
+    ProjectItem(QListWidget *listview, Project *project);
 };
 }  // namespace Ui
 #endif  // PROJECTITEM_H
