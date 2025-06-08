522ade9407370b1fe7ef7caf90e2cac31520314d
mirotvoretts <panoffilya@gmail.com>
2025-06-08 19:18:01 +0300

    chore: prepare for merge, delete client/ui

--- client/ui/main-window/src/projectitem.cpp
+++
@@ -1,8 +0,0 @@
-#include "projectitem.h"
-#include <QListWidget>
-
-namespace Ui {
-ProjectItem::ProjectItem(QListWidget *list_view, Project *project)
-    : project_(project), QListWidgetItem(project->title().c_str(), list_view) {
-}
-}  // namespace Ui
